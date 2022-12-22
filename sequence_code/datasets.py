a738ebd8a17759b36741b8d784e94dc9e0a8360a
hobbang2 <cattie@pukyong.ac.kr>
2022-12-22 22:50:21 +0000

    feat: #9 Add dataset using cloze

--- sequence_code/datasets.py
+++ sequence_code/datasets.py
@@ -224,3 +224,50 @@ class SASRecDataset(Dataset):
 
     def __len__(self):
         return len(self.user_seq)
+
+
+class ClozeDataSet(Dataset):
+    def __init__(self, user_train, num_user, num_item, max_len, mask_prob):
+        self.user_train = user_train
+        self.num_user = num_user
+        self.num_item = num_item
+        self.max_len = max_len
+        self.mask_prob = mask_prob
+
+    def __getitem__(self, user): 
+        # iterator를 구동할 때 사용됩니다.
+        seq = self.user_train[user]
+        tokens = []
+        labels = []
+        for s in seq:
+            prob = np.random.rand() # TODO1: numpy를 사용해서 0~1 사이의 임의의 값을 샘플링하세요.
+            if prob < self.mask_prob:
+                prob /= self.mask_prob
+
+                # BERT 학습
+                # random 하게 80% 를 mask token 으로 변환 
+                if prob < 0.8:
+                    # masking
+                    tokens.append(self.num_item + 1)  # mask_index: num_item + 1, 0: pad, 1~num_item: item index
+                # random 하게 10% 를 random token 으로 변환 
+                elif prob < 0.9:
+                    tokens.append(np.random.randint(1, self.num_item+1))  # item random sampling
+                else:
+                # 나머지 10% 를 original token 으로 사용
+                    tokens.append(s)
+                labels.append(s)  # 학습에 사용
+            else:
+                tokens.append(s)
+                labels.append(0)  # 학습에 사용 X, trivial
+        tokens = tokens[-self.max_len:]
+        labels = labels[-self.max_len:]
+        mask_len = self.max_len - len(tokens)
+
+        # zero padding
+        tokens = [0] * mask_len + tokens
+        labels = [0] * mask_len + labels
+        return torch.LongTensor(tokens), torch.LongTensor(labels)
+    
+    def __len__(self):
+        # 총 user의 수 = 학습에 사용할 sequence의 수
+        return self.num_user
