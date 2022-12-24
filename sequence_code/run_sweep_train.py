10f0fb57db5a0d00987311bb788dde376306d623
kuuneeee <rjsdud0586@gmail.com>
2022-12-24 23:54:08 +0900

    feat: Apply sweep train

---
+++ sequence_code/run_sweep_train.py
@@ -0,0 +1,120 @@
+import argparse
+import os
+
+import numpy as np
+import torch
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+
+from datasets import SASRecDataset
+from models import S3RecModel
+from trainers import FinetuneTrainer
+from utils import (
+    EarlyStopping,
+    check_path,
+    get_item2attribute_json,
+    get_user_seqs,
+    set_seed,
+)
+
+import wandb
+from args import parse_args
+
+
+def main(args):
+    set_seed(args.seed)
+    check_path(args.output_dir)
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
+    args.cuda_condition = torch.cuda.is_available() and not args.no_cuda
+
+    args.data_file = args.data_dir + "train_ratings.csv"
+    item2attribute_file = args.data_dir + args.data_name + "_item2attributes.json"
+
+    user_seq, max_item, valid_rating_matrix, test_rating_matrix, _ = get_user_seqs(
+        args.data_file
+    )
+
+    item2attribute, attribute_size = get_item2attribute_json(item2attribute_file)
+
+    args.item_size = max_item + 2
+    args.mask_id = max_item + 1
+    args.attribute_size = attribute_size + 1
+
+    # save model args
+    args_str = f"{args.model_name}-{args.data_name}"
+    args.log_file = os.path.join(args.output_dir, args_str + ".txt")
+    print(str(args))
+
+    args.item2attribute = item2attribute
+    # set item score in train set to `0` in validation
+    args.train_matrix = valid_rating_matrix
+
+    # save model
+    checkpoint = args_str + ".pt"
+    args.checkpoint_path = os.path.join(args.output_dir, checkpoint)
+
+    train_dataset = SASRecDataset(args, user_seq, data_type="train")
+    train_sampler = RandomSampler(train_dataset)
+    train_dataloader = DataLoader(
+        train_dataset, sampler=train_sampler, batch_size=args.batch_size
+    )
+
+    eval_dataset = SASRecDataset(args, user_seq, data_type="valid")
+    eval_sampler = SequentialSampler(eval_dataset)
+    eval_dataloader = DataLoader(
+        eval_dataset, sampler=eval_sampler, batch_size=args.batch_size
+    )
+
+    test_dataset = SASRecDataset(args, user_seq, data_type="test")
+    test_sampler = SequentialSampler(test_dataset)
+    test_dataloader = DataLoader(
+        test_dataset, sampler=test_sampler, batch_size=args.batch_size
+    )
+    
+    # train
+    wandb.login()
+    with wandb.init(project="Movie_Rec_S3Rec_sweep_train", config=vars(args)):
+        model = S3RecModel(args=args)
+
+        trainer = FinetuneTrainer(
+            model, train_dataloader, eval_dataloader, test_dataloader, None, args
+        )
+
+        print(args.using_pretrain)
+        if args.using_pretrain:
+            pretrained_path = os.path.join(args.output_dir, "Pretrain.pt")
+            try:
+                trainer.load(pretrained_path)
+                print(f"Load Checkpoint From {pretrained_path}!")
+
+            except FileNotFoundError:
+                print(f"{pretrained_path} Not Found! The Model is same as SASRec")
+        else:
+            print("Not using pretrained model. The Model is same as SASRec")
+
+        early_stopping = EarlyStopping(args.checkpoint_path, patience=args.patience, verbose=True)
+        for epoch in range(args.epochs):
+            print('#####################################################################')
+            trainer.train(epoch)
+
+            scores, _ = trainer.valid(epoch)
+            
+            # early_stopping(np.array(scores[-1:]), trainer.model)
+            early_stopping(np.array(scores[2]), trainer.model) # RECALL@10
+            if early_stopping.early_stop:
+                print("Early stopping")
+                break
+
+        # trainer.args.train_matrix = test_rating_matrix
+        # print("---------------Change to test_rating_matrix!-------------------")
+
+        # load the best model
+        # trainer.model.load_state_dict(torch.load(os.path.join(args.output_dir, checkpoint.split('/')[-1])))
+        # scores, result_info = trainer.test(0)
+        # wandb.log(result_info, step=epoch+1)
+        # print(result_info)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
